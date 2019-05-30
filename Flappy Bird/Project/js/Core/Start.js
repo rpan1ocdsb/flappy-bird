a90751486061530e63737b901b995aed43f6b8ba
rpan1ocdsb <51001302+rpan1ocdsb@users.noreply.github.com>
2019-05-30 15:10:17 -0400

    Update Start.js

--- Flappy Bird/Project/js/Core/Start.js
+++ Flappy Bird/Project/js/Core/Start.js
@@ -4,5 +4,10 @@ function startGame() {
     x.className = "game2";
   } else {
     x.className = "game1";
+  } var y = document.getElementById("startButton");
+  if (x.className === "game2") {
+    x.className = "game1";
+  } else {
+    x.className = "game2";
   }
 }
